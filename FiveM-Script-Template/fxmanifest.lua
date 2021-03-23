cb1df85983fec8ae01ee267a0ff4f851d9fae979
Drslime7 <49422667+Drslime7@users.noreply.github.com>
2021-03-23 20:56:36 +1100

    Add files via upload

---
+++ FiveM-Script-Template/fxmanifest.lua
@@ -0,0 +1,19 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'DrSlime7'
+description 'Example resource'
+version '1.0.0'
+
+-- What to run
+client_scripts '/client/client.lua'
+
+server_script '/server/server.lua'
+
+-- Extra data can be used as well
+my_data 'one' { two = 42 }
+my_data 'three' { four = 69 }
+
+-- due to Lua syntax, the following works too:
+my_data('nine')({ninety = "nein"})
